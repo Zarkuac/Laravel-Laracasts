82d0bdc81daf1cf894e285b10f6b2e24e0ea47db
ZARKUAC <tsotne.zarkua.1@btu.edu.ge>
2024-08-26 15:32:49 +0400

    edit,update,delete requests

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -1,12 +1,15 @@
 <x-layout>
-<x-slot:heading> 
+<x-slot:heading>
         Job Listings
     </x-slot:heading>
-    
-    <h2 class="font-bold text-lg"> {{ $job['title'] }}</h2>
-    <p> 
 
-     This job pays {{ $job['salary']}} per year.
+    <h2 class="font-bold text-lg"> {{ $job->title }}</h2>
+    <p>
+
+     This job pays {{ $job->salary}} per year.
+    </p>
+    <p class="mt-6">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
     </p>
 
 </x-layout>
