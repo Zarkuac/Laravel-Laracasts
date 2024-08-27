4d75c4ba8b299f6c30892dc1a0130f72005d90d1
ZARKUAC <tsotne.zarkua.1@btu.edu.ge>
2024-08-27 12:33:00 +0400

    authorization +validation

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class SessionController extends Controller
+{
+    public function create()
+    {
+        return view('auth.login');
+    }
+    public function store() {
+        //validate
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if(! Auth::attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Sorry, do not match.'
+            ]);
+        }
+
+        request()->session()->regenerate();
+
+        return redirect('/jobs');
+    }
+    public function destroy() {
+        Auth::logout();
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -9,13 +11,11 @@ Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
 
-//Route::controller(JobController::class)->group(function () {
-//    Route::get('/jobs', 'index');
-//    Route::get('/jobs/create', 'create');
-//    Route::get('/jobs/{job}', 'show');
-//    Route::post('/jobs', 'store');
-//    Route::get('/jobs/{job}/edit', 'edit');
-//    Route::patch('/jobs/{job}', 'update');
-//    Route::delete('/jobs/{job}', 'destroy');
-//});
 
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destroy']);
